29853b5415f58654982e778b299dd89f553cb8e4
kpvishnu-official <kpvishnu.official@gmail.com>
2022-10-08 23:02:16 +0530

    trying php

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Just for training my coding skilll and knowledge">
-    <title>Vishnu Kp || Just for training purpose</title>
-    <link rel="stylesheet" href="style.css">
-</head>
+<php include '/prepend.php'; ?>
+<?php print_r($header); ?>
 <body>
     <div id="Header" class="header">
         <div class="sub-header">
@@ -39,11 +31,4 @@
             </div>
         </div>
     </div>
-    <div class="footer">
-        <div class="sub al-center">
-            <a href="#Header">Vishnu KP</a>
-            <span>&copy;copyright 2022 Vishnu Kp</span>
-        </div>
-    </div>
-</body>
-</html>
+    <?php print_r($footer); ?>
---
+++ prepend.php
@@ -0,0 +1,21 @@
+<?php
+
+$header =  '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Just for training my coding skill and knowledge">
+    <title>Vishnu Kp || Just for training purpose</title>
+    <link rel="stylesheet" href="style.css">
+</head>';
+
+$footer = '    <div class="footer">
+<div class="sub al-center">
+    <a href="#Header">Vishnu KP</a>
+    <span>&copy;copyright 2022 Vishnu Kp</span>
+</div>
+</div>
+</body>
+</html>';
