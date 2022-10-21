095e79891bae7608a307d6585b248f5ed5751f78
kpvishnu-official <kpvishnu.official@gmail.com>
2022-10-21 20:28:03 +0530

    table of nakshatras

--- formSubmitPage.php
+++ formSubmitPage.php
@@ -1,18 +1,44 @@
 <?php
+
+ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
 include __DIR__ . '/client.php';
 
-$client = new Prokerala\Api\Sample\ApiClient('YOUR_CLIENT_ID', 'YOUR_CLIENT_SECRET');
+$date = new DateTimeImmutable($_GET['date-time']);
+$client = new Prokerala\Api\Sample\ApiClient('ed88f91f-accf-4215-ac57-0af56fe3f3bc', 'IAuePswWvP0BFERt7gLCko9w1uUHXRnNzpSJPjPM');
 
 /**
  * $timestamp = new DateTime();
  * echo $timestamp->format('c'); // Returns ISO8601 in proper format
  * echo $timestamp->format(DateTime::ISO8601); // Works the same since const ISO8601 = "Y-m-d\TH:i:sO"
  */
-$data = $client->get('v2/astrology/birth-details', [
+$dataObject = $client->get('v2/astrology/birth-details', [
     'ayanamsa' => 1,
     'coordinates' => '23.1765,75.7885',
-    'datetime' => '2020-10-19T12:31:14+00:00'
+    'datetime' => $date->format('Y-m-d\TH:i:sP')
 ]);
 
-print_r($data);
+$data = $dataObject->data->nakshatra;
 ?>
+<?php include 'header.php' ?>
+
+<table>
+    <thead>
+        <tr>
+            <th>Nakshatra</th>
+        </tr>
+    </thead>
+    <tr>
+        <th>Nakshatra</th>
+        <td><?=$data->name?></td>
+    </tr>
+    <tr>
+        <th>Pada</th>
+        <td><?=$data->pada?></td>
+    </tr>
+    <tr>
+        <th>Nakshatra Lord</th>
+        <td><?=$data->lord->name?>(<?=$data->lord->vedic_name?>)</td>
+    </tr>
+</table>
+
+<?php include 'footer.php' ?>
--- index.php
+++ index.php
@@ -1,4 +1,44 @@
 <?php include 'header.php' ?>
+<?php
+    function geoLocate($address)
+    {
+        try {
+            $lat = 0;
+            $lng = 0;
+    
+            $data_location = "https://maps.google.com/maps/api/geocode/json?key=".$GOOGLE_API_KEY_HERE."&address=".str_replace(" ", "+", $address)."&sensor=false";
+            $data = file_get_contents($data_location);
+            usleep(200000);
+            // turn this on to see if we are being blocked
+            // echo $data;
+            $data = json_decode($data);
+            if ($data->status=="OK") {
+                $lat = $data->results[0]->geometry->location->lat;
+                $lng = $data->results[0]->geometry->location->lng;
+    
+                if($lat && $lng) {
+                    return array(
+                        'status' => true,
+                        'lat' => $lat, 
+                        'long' => $lng, 
+                        'google_place_id' => $data->results[0]->place_id
+                    );
+                }
+            }
+            if($data->status == 'OVER_QUERY_LIMIT') {
+                return array(
+                    'status' => false, 
+                    'message' => 'Google Amp API OVER_QUERY_LIMIT, Please update your google map api key or try tomorrow'
+                );
+            }
+    
+        } catch (Exception $e) {
+    
+        }
+    
+        return array('lat' => null, 'long' => null, 'status' => false);
+    }
+?>
 <body>
     <div id="Header" class="header">
         <div class="sub-header">
@@ -22,11 +62,13 @@
                         </div>
                     </div>
                 </div>
-
-                <form action="formSubmitPage.php" method="post">
-                    <label>Name: <input type="text" required></label>
-                    <label>Date of Birth: <input type="birth" required></label>
-                    <label>Location: <select><option>Select any number</option></select></label>
+                <form action="/formSubmitPage.php" method="GET">
+                    <label>Name: <input type="submit">ut type="text" required></label>
+                    <label for="">
+                        Date : 
+                        <input type="datetime-local" name="date-time" id="date-time">
+                    </label>
+                    <input type="hidden" name="process" value='1'>
                     <input type="submit">
                 </form>
                 
